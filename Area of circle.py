d0d57c111ff21fabe641ecb9ec2c6e5011bc10fd
Patel Yash Pankajbhai <71980446+patelyash9775@users.noreply.github.com>
2021-02-14 17:07:07 +0530

    Print float value of area for circle

--- Area of circle.py
+++ Area of circle.py
@@ -1,5 +1,5 @@
 import math
 
-r=float(input("The radius of the circle is : "))
+r=float(input("The radius of the circle is: "))
 a=math.pi*r*r
 print("The area of the circle with radius ",r," is : ",a,end="")

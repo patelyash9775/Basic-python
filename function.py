745459f2be9265977ab989ca836e899739a2ef35
Patel Yash Pankajbhai <71980446+patelyash9775@users.noreply.github.com>
2021-02-14 16:41:24 +0530

    Creating function

---
+++ function.py
@@ -0,0 +1,11 @@
+#function definition
+def print_sample(exp):
+    print(exp)
+    
+def return_sample(exp):
+    return exp
+    
+print_sample("Hello! Sample function")
+
+a=return_sample("Return value")
+print(a)

7d7e32272bebb87c3fcc3aead321bab3fac876e3
Patel Yash Pankajbhai <71980446+patelyash9775@users.noreply.github.com>
2021-02-15 19:06:31 +0530

    New syntax of print statement to print numbers in decimal , float , octal , hexa (similar to c lan)

---
+++ Print statement for representation of number to other.py
@@ -0,0 +1,7 @@
+a=int(input("Enter a number : "))
+
+print ("Actual Number = %d" %a)
+print ("Float of the number = %f" %a)
+print ("Octal equivalent of the number = %o" %a)
+print ("Hexadecimal equivalent of the number = %x" %a)
+print ("Exponential equivalent of the number = %e" %a)

2ecb392ac7749b8a9081ddc088517884e17a3015
Patel Yash Pankajbhai <71980446+patelyash9775@users.noreply.github.com>
2021-02-15 19:03:09 +0530

    Converting in decimal , octal , hexadecimal

---
+++ Different types of converter(representation) of number.py
@@ -0,0 +1,10 @@
+#Decimal to octal,hexadecimal and binary
+a=int(input("Enter a number : "))
+print("Hexadecimal of ",a," is ",hex(a))
+print("Octal of ",a," is ",oct(a))
+print("Binary of ",a," is ",bin(a))
+
+#Octal,hexadecimal and binary to decimal
+print (int('010',8))
+print (int('0xaa',16))
+print (int('1010',2))

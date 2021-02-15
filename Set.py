186eb55f5f367a1ee842eb983b1adcb420fc343b
Patel Yash Pankajbhai <71980446+patelyash9775@users.noreply.github.com>
2021-02-15 18:59:41 +0530

    Some operation in set

--- Set.py
+++ Set.py
@@ -2,10 +2,18 @@ mySet={"C","C++","Java"}
 
 print(mySet)
 
+print()
 print("Length of set is ",len(mySet))
 
-mySet.add("HTML")
+print()
+mySet.add("Python")
 print("After adding : ",mySet)
 
+print()
 mySet.remove("C++")
 print("After removing: ",mySet)
+
+print()
+mySet.pop() #Pop fun takes no argument for set
+print("After poping : ",mySet)
+
